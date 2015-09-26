bb70de74283d8702ee1bb550ad0cb4fd15fab781
Gates33 <jedi.gates@gmail.com>
2015-09-26 18:10:51 +0200

    Update default.py

--- default.py
+++ default.py
@@ -270,9 +270,9 @@ def getStreamUrl(id):
         return ""
     
     else:
-        get_json_code = re.compile(r'dmp\.create\(document\.getElementById\(\'player\'\),\s*(\{.*?)"\}\]\}.*\}\);').findall(content)[0]
-        get_json_code += '"}]}}}'
-        print get_json_code
+        get_json_code = re.compile(r'dmp\.create\(document\.getElementById\(\'player\'\),\s*([^);]+)').findall(content)[0]
+        # print get_json_code
+        
         cc= json.loads(get_json_code)['metadata']['qualities']  #['380'][0]['url']
         print cc
         if '1080' in cc.keys():
